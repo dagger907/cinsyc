3c83bc182a6b5d4b4d4afc0fa8ec03f7c88bdaee
U-BOK\bl-eyo <bl-eyo@BL-0070.BOK.LOCAL>
2014-12-11 12:39:23 +0000

    Added autoall.sh which is the script you run after changing scripts. Run either manually or through cron.

---
+++ autoall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#All credits to Eyolv André Øverland
+pathvar=your path to syscollect-folder
+$pathvar/tarsyscollect.sh
+$pathvar/dochecksys.sh
+$pathvar/sendwarnings.sh
+$pathvar/dosendwarnings.sh
