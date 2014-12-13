11390ee59a104ac857b0867b68d42aac63d3b1ee
Eyolv A. Øverland <eyolvoe@gmail.com>
2014-12-13 03:42:03 +0100

    Some small changes to code.

--- dosendwarnings.sh
+++ dosendwarnings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #All credit to Eyolv André Øverland
 #NOTE: You have to edit the email addresses to fit your setup
-pathvar=/home/localadmin/syscollect
+pathvar=path to your syscollect-folder
 cat $pathvar/tmp/mailreport.txt |  mail -r syscollect@yourdomain -s "Overview of updates for Linux - Syscollect" youremail@yourdomain.com
