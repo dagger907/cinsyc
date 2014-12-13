f0d52b529556917498caeb9ab817c8d0fdd64bea
Eyolv A. Øverland <eyolvoe@gmail.com>
2014-12-13 03:40:52 +0100

    Some small changes to code.

--- tarsyscollect.sh
+++ tarsyscollect.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#All credits to Eyolv André Øverland
 pathvar=your path to syscollect-folder
 tar Jcvf $pathvar/tarsyscollect/syscollect_`date +%Y%m-%d`.tar.xz $pathvar/syscollect/*  && rm $pathvar/syscollect/*
 
